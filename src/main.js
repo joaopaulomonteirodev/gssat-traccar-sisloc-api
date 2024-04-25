15041ec1186827673ef693433b5c09c396e20f9a
daniellemuniz <mdanielle249@gmail.com>
2024-04-25 08:46:56 -0300

    log error

--- src/main.js
+++ src/main.js
@@ -4,9 +4,7 @@ import { sendData } from "./request.js"
 
 const connection = await createConnection()
 
-
-try {
-    const [results] = await connection
+const [results] = await connection
     .query(`SELECT p.deviceid, p.servertime, p.attributes AS 'position_data', d.attributes AS 'device_data'
     FROM tc_positions p
         INNER JOIN tc_devices d ON(d.id = p.deviceid)
@@ -16,29 +14,36 @@ try {
             GROUP BY deviceid
         ) m ON(p.deviceid = m.deviceid AND p.servertime = m.servertime) `)
 
-    const gps = results.map(
-        result => ({
-            servertime: result.servertime,
-            device: JSON.parse(result.device_data),
-            telemetry: JSON.parse(result.position_data)
-        })
-    ).filter(position => !!position.device.Patrimonio)
-    .map(position => ({
-        nr_patrimonio: position.device.Patrimonio,
-        dt_medicao: position.servertime,
-        vl_medicao: position.telemetry.hours
+const gps = results.map(
+    result => ({
+        servertime: result.servertime,
+        device: JSON.parse(result.device_data),
+        telemetry: JSON.parse(result.position_data)
+    })
+).filter(position => !!position.device.Patrimonio)
+.map(position => ({
+    nr_patrimonio: position.device.Patrimonio,
+    dt_medicao: position.servertime,
+    vl_medicao: position.telemetry.hours
 
-    }))
+}))
 
-    const {data} = await sendData({ gps })
+const data_sent = JSON.stringify({ gps })
 
-    const data_sent = JSON.stringify({ gps })
+try {
+    const { data } = await sendData({ gps })
     const response = JSON.stringify(data)
 
     await connection.query(`INSERT INTO SISLOCINTEGRATIONLOG (data_sent, response) VALUES('${data_sent}', '${response}')`)
 
 } catch (error) {
-    console.log(error)
+    const data = (error.response?.data)  ? error.response?.data : { error: 'Something went wrong' }
+
+    const response = JSON.stringify({
+        informacao: data
+    })
+
+    await connection.query(`INSERT INTO SISLOCINTEGRATIONLOG (data_sent, response) VALUES('${data_sent}', '${response}')`)
 } finally {
     connection.end()
 }
