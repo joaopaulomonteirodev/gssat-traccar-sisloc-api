cd6eda8c966d10b6f3e9c2cfe0afd718e5b0bcc6
daniellemuniz <mdanielle249@gmail.com>
2024-05-10 08:42:07 -0300

    testing new hours

--- src/main.js
+++ src/main.js
@@ -1,40 +1,40 @@
 import { createConnection } from "./conection.js"
 import { sendData } from "./request.js"
 
-
+const BR_TIME_OFFSET = 3 * 60 * 60 * 1000
 const connection = await createConnection()
 
 const [results] = await connection
-    .query(`SELECT p.deviceid, p.servertime, p.attributes AS 'position_data', d.attributes AS 'device_data'
+    .query(`SELECT p.deviceid, p.devicetime, p.attributes AS 'position_data', d.attributes AS 'device_data'
     FROM tc_positions p
         INNER JOIN tc_devices d ON(d.id = p.deviceid)
         INNER JOIN (
-            SELECT deviceid, MAX(servertime) AS servertime
+            SELECT deviceid, MAX(devicetime) AS devicetime
             FROM tc_positions
             GROUP BY deviceid
-        ) m ON(p.deviceid = m.deviceid AND p.servertime = m.servertime) `)
+        ) m ON(p.deviceid = m.deviceid AND p.devicetime = m.devicetime) `)
 
 const gps = results.map(
     result => ({
-        servertime: result.servertime,
+        devicetime: new Date(result.devicetime.getTime() - BR_TIME_OFFSET).toISOString(),
         device: JSON.parse(result.device_data),
         telemetry: JSON.parse(result.position_data)
     })
 ).filter(position => !!position.device.Patrimonio)
 .map(position => ({
     nr_patrimonio: position.device.Patrimonio,
-    dt_medicao: position.servertime,
+    dt_medicao: position.devicetime.replace('0Z', 'Z'),
     vl_medicao: position.telemetry.hours
-
 }))
 
 const data_sent = JSON.stringify({ gps })
 
 try {
-    const { data } = await sendData({ gps })
-    const response = JSON.stringify(data)
+    gps.map(g => console.log(g.dt_medicao))
+    // const { data } = await sendData({ gps })
+    // const response = JSON.stringify(data)
 
-    await connection.query(`INSERT INTO SISLOCINTEGRATIONLOG (data_sent, response) VALUES('${data_sent}', '${response}')`)
+    // await connection.query(`INSERT INTO SISLOCINTEGRATIONLOG (data_sent, response) VALUES('${data_sent}', '${response}')`)
 
 } catch (error) {
     const data = (error.response?.data)  ? error.response?.data : { error: 'Something went wrong' }
