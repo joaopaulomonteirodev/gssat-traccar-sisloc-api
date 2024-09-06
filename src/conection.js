28a7cfcd603b1f9d632127c83a7ebf04340ff40a
Joao Paulo Monteiro <joaopaulomonteirodev@gmail.com>
2024-09-06 00:39:02 +0000

    fix postgre errors

--- src/conection.js
+++ src/conection.js
@@ -1,4 +1,5 @@
-import { Client } from 'pg'
+import pg from 'pg'
+const { Client } = pg
 
 export const createConnection = async () => {
     const client = new Client({
--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createConnection } from "./conection.js"
 import { sendData } from "./request.js"
 
+const MILISEC_TO_HOUR = 1000 * 60 * 60
+
+const toHours = (timeinmili) => (timeinmili / MILISEC_TO_HOUR).toFixed(1)
+
 const BR_TIME_OFFSET = 3 * 60 * 60 * 1000
 const connection = await createConnection()
 
 const { rows: results } = await connection
-    .query(`SELECT p.deviceid, p.devicetime, p.attributes AS 'position_data', d.attributes AS 'device_data'
+    .query(`SELECT p.deviceid, p.devicetime, p.attributes AS "position_data", d.attributes AS "device_data"
     FROM tc_positions p
         INNER JOIN tc_devices d ON(d.id = p.deviceid)
         INNER JOIN (
@@ -24,7 +28,7 @@ const gps = results.map(
 .map(position => ({
     nr_patrimonio: position.device.Patrimonio,
     dt_medicao: position.devicetime.replace('0Z', 'Z'),
-    vl_medicao: position.telemetry.hours
+    vl_medicao: toHours(position.telemetry.hours)
 }))
 
 const data_sent = JSON.stringify({ gps })
